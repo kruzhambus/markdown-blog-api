39fe4764150ace96554b8e3a801e4e5f9f7e7d29
kruzhambus <maks.mrug.ru@gmail.com>
2022-07-12 12:59:58 +0000

    fix tab

--- index.js
+++ index.js
@@ -6,34 +6,34 @@ import mongoose from "mongoose"
 dotenv.config()
 
 mongoose
-		.connect(`mongodb+srv://admin:${process.env.MongoDBPassword}@cluster0.htchcrj.mongodb.net/?retryWrites=true&w=majority`)
-		.then(() => console.log('DB Connected'))
-		.catch((err) => console.log('DB Connected failed: ', err))
+	.connect(`mongodb+srv://admin:${process.env.MongoDBPassword}@cluster0.htchcrj.mongodb.net/?retryWrites=true&w=majority`)
+	.then(() => console.log('DB Connected'))
+	.catch((err) => console.log('DB Connected failed: ', err))
 
 const app = express()
 
 app.use(express.json())
 
 app.get('/', (req, res) => {
-		res.send('MarkDown-BLOG!')
+	res.send('MarkDown-BLOG!')
 })
 
 app.post('/auth/login', (req, res) => {
-		const token = jwt.sign({
-				email: req.body.email,
-				password: req.body.password
-		}, process.env.JWT_SECRET)
+	const token = jwt.sign({
+			email: req.body.email,
+			password: req.body.password
+	}, process.env.JWT_SECRET)
 
-		res.json({
-				success: true,
-				token,
-		})
+	res.json({
+		success: true,
+		token,
+	})
 })
 
 app.listen(4444, (err) => {
-		if (err) {
-				return console.log(err)
-		}
+	if (err) {
+		return console.log(err)
+	}
 
-		console.log('Server OK')
+	console.log('Server OK')
 })

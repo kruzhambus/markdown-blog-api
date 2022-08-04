5c8d6b8c5034cff3f5f965c403c53fd5559bdf01
kruzhambus <maks.mrug.ru@gmail.com>
2022-08-04 11:43:52 -0400

    refactoring

--- index.js
+++ index.js
@@ -1,39 +1,69 @@
-import dotenv from 'dotenv'
-import express from "express"
-import jwt from "jsonwebtoken"
-import mongoose from "mongoose"
+import express from 'express';
+import fs from 'fs';
+import multer from 'multer';
+import cors from 'cors';
+import { config } from 'dotenv';
+import mongoose from 'mongoose';
+import { registerValidation, loginValidation, postCreateValidation } from './validations.js';
+import { handleValidationErrors, checkAuth } from './utils/index.js';
+import { UserController, PostController } from './controllers/index.js';
 
-dotenv.config()
-
+config();
 mongoose
-	.connect(`mongodb+srv://admin:${process.env.MongoDBPassword}@cluster0.htchcrj.mongodb.net/?retryWrites=true&w=majority`)
-	.then(() => console.log('DB Connected'))
-	.catch((err) => console.log('DB Connected failed: ', err))
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log('DB ok'))
+  .catch((err) => console.log('DB error', err));
 
-const app = express()
+const app = express();
 
-app.use(express.json())
+const storage = multer.diskStorage({
+  destination: (_, __, cb) => {
+    if (!fs.existsSync('uploads')) {
+      fs.mkdirSync('uploads');
+    }
+    cb(null, 'uploads');
+  },
+  filename: (_, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
 
-app.get('/', (req, res) => {
-	res.send('MarkDown-BLOG!')
-})
+const upload = multer({ storage });
 
-app.post('/auth/login', (req, res) => {
-	const token = jwt.sign({
-			email: req.body.email,
-			password: req.body.password
-	}, process.env.JWT_SECRET)
+app.use(express.json());
+app.use(cors());
+app.use('/uploads', express.static('uploads'));
 
-	res.json({
-		success: true,
-		token,
-	})
-})
+app.post('/auth/login', loginValidation, handleValidationErrors, UserController.login);
+app.post('/auth/register', registerValidation, handleValidationErrors, UserController.register);
+app.get('/auth/me', checkAuth, UserController.getMe);
 
-app.listen(4444, (err) => {
-	if (err) {
-		return console.log(err)
-	}
+app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
+  res.json({
+    url: `/uploads/${req.file.originalname}`,
+  });
+});
 
-	console.log('Server started')
-})
+app.get('/tags', PostController.getLastTags);
+
+app.get('/posts', PostController.getAll);
+app.get('/posts/tags', PostController.getLastTags);
+app.get('/posts/:id', PostController.getOne);
+app.post('/posts', checkAuth, postCreateValidation, handleValidationErrors, PostController.create);
+app.delete('/posts/:id', checkAuth, PostController.remove);
+app.patch(
+  '/posts/:id',
+  checkAuth,
+  postCreateValidation,
+  handleValidationErrors,
+  PostController.update,
+);
+
+app.listen(process.env.PORT || 4444, (err) => {
+  if (err) {
+    return console.log(err);
+  }
+
+  console.log('Server OK');
+});
+
